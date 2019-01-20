e7901e9ec722ac1860ab3730d47610baba8c9a5f
Santhosh Babu M R <santhosh.infotec@gmail.com>
2019-01-20 12:57:16 +0530

    UPDATED

---
+++ javaindepth/src/com/semanticsquare/Packages/FullQualifiedClassName.java
@@ -0,0 +1,6 @@
+class FullQualifiedClassName {
+	public static void main(String... args) {
+		java.util.Date date = new java.util.Date();
+		java.sql.Date sdate = new java.sql.Date(15479689);
+	}
+}
---
+++ javaindepth/src/com/semanticsquare/Packages/Import.java
@@ -0,0 +1,10 @@
+// Explicit import preferred over * import for better clarity
+import java.util.Date;
+import java.util.ArrayList;
+
+class Import {
+	public static void main(String... args) {
+		Date date;
+		ArrayList arrayList;
+	}
+}
