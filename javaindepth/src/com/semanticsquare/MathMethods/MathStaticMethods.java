5a1d1dfada97df75096c7bbd5016a746cb993e96
Santhosh Babu M R <santhosh.infotec@gmail.com>
2019-02-09 12:00:11 +0530

    updated

---
+++ javaindepth/src/com/semanticsquare/MathMethods/MathStaticMethods.java
@@ -0,0 +1,37 @@
+package com.semanticsquare.MathMethods;
+
+class MathStaticMethods {
+	public static void main(String[] args) {
+		
+		System.out.println("");
+		System.out.println("Math.random(): " + Math.random());
+		System.out.println("Generate random number btw 0 and 5 >> (int) (Math.random()*5): " + (int) (Math.random()*5));
+		
+		System.out.println("");
+		System.out.println("Math.abs(-240): " + Math.abs(-240));
+		
+		System.out.println("");
+		System.out.println("Math.round(24.8): " + Math.round(24.8));
+		System.out.println("Math.round(24.25f): " + Math.round(24.25f));
+		
+		System.out.println("");
+		System.out.println("Math.ceil(20.1): " + Math.ceil(20.1));
+		
+		System.out.println("");
+		System.out.println("Math.floor(24.8): " + Math.floor(24.8));
+		
+		System.out.println("");
+		System.out.println("0.00/0.00: " + 0.00/0.00);
+		
+		System.out.println("");
+		System.out.println("Math.sqrt(-4.0): " + Math.sqrt(-4.0));
+		System.out.println("Math.sqrt(16.0): " + Math.sqrt(16.0));
+		
+		System.out.println("");
+		System.out.println("Math.max(-4, 12): " + Math.max(-4, 12));
+		
+		System.out.println("");
+		System.out.println("Math.min(-4, 12): " + Math.min(-4, 12));
+		
+	}
+}
