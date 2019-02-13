61457c4ad2e45b21cfc5a5df87c1de16af74532d
Santhosh Babu M R <santhosh.infotec@gmail.com>
2019-02-13 08:42:49 +0530

    updated

--- javaindepth/src/com/semanticsquare/ExpensiveBoxedPrimitive/CompareBoxPrimitive.java
+++ javaindepth/src/com/semanticsquare/ExpensiveBoxedPrimitive/CompareBoxPrimitive.java
@@ -4,6 +4,10 @@ class CompareBoxPrimitive {
 
 		Integer n1 = new Integer(1);
 		Integer n2 = new Integer(1);
+		String s1 = new String("hello");
+		String s2 = new String("hello");
+		String s3 = "world";
+		String s4 = "world";
 		
 		System.out.println("");
 		System.out.println("box primitive: n1 == n2: " + (n1 == n2));
@@ -12,8 +16,14 @@ class CompareBoxPrimitive {
 		System.out.println("");
 		System.out.println("box primitive: n1.equals(n2): " + n1.equals(n2));
 		System.out.println("");
+		System.out.println("String object: s1 == s2: " + (s1 == s2));
+		System.out.println("");
+		System.out.println("String object: s1.equals(s2): " + (s1.equals(s2)));
+		System.out.println("");
+		System.out.println("String primitive: s3 == s4: " + (s3 == s4));
 		
 		Integer n3 = new Integer(11);
+		System.out.println("");
 		System.out.println("box primitive: n1 < n3: " + (n1 < n3)); // does auto unboxing
 		System.out.println("");
 		
